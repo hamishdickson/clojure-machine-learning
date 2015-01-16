987c1dc5e0fbceb3f85ca2fd7fd4a50ddef70963
mishyTheBear <hamish.dickson@gmail.com>
2015-01-16 08:57:57 +0000

    plot multivar

--- src/clojure_machine_learning/linear-regression/multivar-linear-regression.clj
+++ src/clojure_machine_learning/linear-regression/multivar-linear-regression.clj
@@ -1,6 +1,10 @@
 (ns multivar-linear-regression
-	(:use [incanter.datasets :only [get-dataset]]
-		[incanter.core :only [sel to-matrix]]))
+  (:use [clojure.core.matrix :only [column-count]]
+        [incanter.datasets :only [get-dataset]]
+        [incanter.charts :only [scatter-plot xy-plot add-points]]
+        [incanter.stats :only [linear-model]]
+        [incanter.core :only [view sel to-matrix]])
+  (:require [clatrix.core :as cl]))
 
 ;; can fetch the iris dataset like this
 (def iris
@@ -8,3 +12,13 @@
 
 (def X (sel iris :cols (range 1 5)))
 (def Y (sel iris :cols 0))
+
+(def iris-linear-model
+  (linear-model Y X))
+
+(defn plot-iris-linear-model []
+  (let [x (range -100 100)
+        y (:fitted iris-linear-model)]
+    (view (xy-plot x y :x-label "X" :y-label "Y"))))
+
+(
