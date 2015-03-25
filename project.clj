2fe02d9cd15a52f6ef5cbd557b247d854bed3272
hamishdickson <hamish.dickson@gmail.com>
2015-03-25 21:28:44 +0000

    update description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-machine-learning "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure for machine learning"
+  :url "https://github.com/hamishdickson/clojure-machine-learning"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
